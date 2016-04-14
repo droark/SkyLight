e238c6033e5a682fef25dbf9ca5b5ee5f7a1b413
Chris Pitzer <cpitzer@mobilepaks.com>
2016-04-13 23:38:06 -0700

    Hue scroll

--- arduino_scripts/rotary_encoder_test/rotary_encoder_test.ino
+++ arduino_scripts/rotary_encoder_test/rotary_encoder_test.ino
@@ -1,5 +1,6 @@
 
 #include <Encoder.h>
+#include "utility/HSBconvert/HSBColor.h"
 #include <OctoWS2811.h>
 
 Encoder knob(0, 1);
@@ -8,7 +9,6 @@ const int ledsPerStrip = 10;
 
 DMAMEM int displayMemory[ledsPerStrip*6];
 int drawingMemory[ledsPerStrip*6];
-
 struct int24{
     unsigned int data : 24;
 };
@@ -42,7 +42,7 @@ Do this ten times...
 int NOLED = 255;
 
 int ledloop[28] = {
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 19, 29, 39, 49, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 40, 30, 20, 10
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 };
 
 int LOOP_LENGTH = 28;
@@ -51,10 +51,20 @@ int old_color = 28;
 int new_color = 0;
 
 long old_knob = 0;
+int hue = 0;
+int toRGB[3];
 
+int convertArrayTo32b(int *array){
+  int ret = 0;
+  for(int i=0;i<3;i++){
+    ret += (array[i] << 8*i);
+  }
+  return ret;
+}
 
 void loop() {
 
+/*
   long new_knob = knob.read();
   if (new_knob != old_knob) {
     old_color = new_color;
@@ -73,11 +83,19 @@ void loop() {
       new_color = LOOP_LENGTH-1;
     }
   }
+*/
+  hue += 1;
+  hue = hue % 360;
 
-  leds.setPixel(ledloop[new_color], PINK);
-  leds.setPixel(ledloop[old_color], BLACK);
+  H2R_HSBtoRGB(hue, 99, 99, toRGB);
+  int color = convertArrayTo32b(toRGB);
+  
+
+  leds.setPixel(0, color);
+  
+  //leds.setPixel(ledloop[old_color], BLACK);
 
   leds.show();
-  //int microsec = 100000; // 2000000 / leds.numPixels();  // change them all in 2 seconds
-  //delayMicroseconds(microsec);
+  int microsec = 10000; // 2000000 / leds.numPixels();  // change them all in 2 seconds
+  delayMicroseconds(microsec);
 }
